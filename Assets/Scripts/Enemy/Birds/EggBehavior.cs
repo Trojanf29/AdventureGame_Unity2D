ba435e9a34b767b039b11f2e0b2ab15eacbf65f3
andn15050906 <antrongdn2022@gmail.com>
2024-07-06 18:53:44 +0700

    Trojanf29 - Add Ranged Boss

---
+++ Assets/Scripts/Enemy/Birds/EggBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EggTime : MonoBehaviour
+{
+    [SerializeField]
+    private float eggLifetime = 2f;
+
+    void Start()
+    {
+        StartCoroutine(DestroyEggAfterTime());
+    }
+
+    IEnumerator DestroyEggAfterTime()
+    {
+        yield return new WaitForSeconds(eggLifetime);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy/Boss/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float spawnRadius = 5f;
+
+    [SerializeField]
+    private float minSpawnInterval = 1f;
+
+    [SerializeField]
+    private float maxSpawnInterval = 3f;
+
+    [SerializeField]
+    private float enemyLifetime = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Vector3 spawnPosition = GetRandomSpawnPosition();
+            GameObject newEnemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            StartCoroutine(DestroyEnemyAfterDelay(newEnemy, enemyLifetime));
+
+            float spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        Vector2 randomPoint = Random.insideUnitCircle * spawnRadius;
+        return transform.position + new Vector3(randomPoint.x, randomPoint.y, 0);
+    }
+
+    IEnumerator DestroyEnemyAfterDelay(GameObject enemy, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (enemy != null)
+        {
+            Destroy(enemy);
+        }
+    }
+}
